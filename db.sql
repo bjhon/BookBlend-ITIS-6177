0b6d1c00862ab86725d5b1e487b8e27cd650180a
Bianca Jhonson <biancajhonson@gmail.com>
2024-05-06 22:14:06 -0400

    created server database to hold book reviews

---
+++ db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS db;
+
+USE db;
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    review TEXT NOT NULL
+);
+
+INSERT INTO reviews (id, review) VALUES
+(1, 'The book was excellent! I couldn''t put it down.'),
+(2, 'I found the characters in the book to be very engaging.'),
+(3, 'The plot of the book was a bit predictable, but overall it was enjoyable.'),
+(4, 'I didn''t like the writing style of the book; it felt too simplistic.');
